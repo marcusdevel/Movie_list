daf8b977f8a947079963b772fbdb29cc2f522bd9
Marcus <marcusviniciusjp@hotmail.com>
2024-03-05 17:55:45 -0300

    Testando api do card Batman.

--- Movie_list/src/App.jsx
+++ Movie_list/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   return (
     <>
+
+    <img src="https://m.media-amazon.com/images/M/MV5BZWQ0OTQ3ODctMmE0MS00ODc2LTg0ZTEtZWIwNTUxOGExZTQ4XkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg"/>
       <h1>Movie_list</h1>
     </>
   );
