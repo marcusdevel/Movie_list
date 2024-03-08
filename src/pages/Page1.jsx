3b46be5d4a0e2084a76f04658433fad34929893f
Marcus <marcusviniciusjp@hotmail.com>
2024-03-08 15:27:57 -0300

    import de estado de filme.

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-
+import React, { useState } from "react";
 const Page1 = () => {
   return (
-    <div className="mainPage">
+    <div>
       <h1>All Films</h1>
       <form>
-        <input id="pesquisa" placeholder="Search" />
+        <input id="pesquisa" placeholder="Enter a movie title" />
         <button>Pesquisar</button>
       </form>
-      <img
-        src="https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-        alt=""
-      />
+      <div className="mainPage">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+          alt=""
+        />
 
-      <div className="card2">
-        <button type="button">View Details</button>
-        <button type="button">Watchlist</button>
+        <div className="card2">
+          <button type="button">View Details</button>
+          <button type="button">Watchlist</button>
+        </div>
       </div>
     </div>
   );
