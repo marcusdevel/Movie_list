20006eed9017bcdc04e8853a931013f403c28c7b
Marcus <marcusviniciusjp@hotmail.com>
2024-03-08 15:32:39 -0300

    estados para título e detalhes do filme.

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import React, { useState } from "react";
 const Page1 = () => {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieDetails, setMovieDetails] = useState(null);
   return (
     <div>
       <h1>All Films</h1>
