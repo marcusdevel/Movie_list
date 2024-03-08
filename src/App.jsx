9058abee05e0f3942bedd4c3dbdf3e967b6ebf44
Marcus <marcusviniciusjp@hotmail.com>
2024-03-08 10:47:12 -0300

    inserções dos componentes Header e Page1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "./styles/Header2.css";
 import "./App.css";
-
+import Page1 from "./pages/Page1";
+import Header from "./components/header";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-
-    <img src="https://m.media-amazon.com/images/M/MV5BZWQ0OTQ3ODctMmE0MS00ODc2LTg0ZTEtZWIwNTUxOGExZTQ4XkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_SX300.jpg"/>
+    <div>
+      
+      <Header />
+      <Page1 />
       <h1>Movie_list</h1>
-    </>
+    </div>
   );
 }
 
