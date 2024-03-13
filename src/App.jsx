ac46ab1aa4f41cef041223d1d6a6c215aa10ccee
Marcus <marcusviniciusjp@hotmail.com>
2024-03-13 00:36:18 -0300

    rotas para as páginas watchlist e page1..

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import "./App.css";
 import Page1 from "./pages/Page1";
 import Header from "./components/header";
 import WatchList from "./pages/WatchList";
+import React, { useState } from "react";
 function App() {
+  const [watchlist, setWatchlist] = useState([]);
   return (
     <BrowserRouter>
       <div>
         <Header />
       </div>
       <Routes>
-        <Route path="/WatchList" element={<WatchList />} />
-        <Route path="/" element={<Page1 />} />
+        <Route path="/WatchList" element={<WatchList watchlist={watchlist} />} />
+        <Route path="/" element={<Page1 watchlist={watchlist} setWatchlist={setWatchlist}/>} />
       </Routes>
     </BrowserRouter>
   );
